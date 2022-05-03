e000c38f12b3350dfaa064ae0391ebdec0f18c6f
Ben DiNatale96 <bend6991@gmail.com>
2022-05-03 18:59:28 -0400

    giphy api javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,39 @@
-const q = 'cats';
-const apiKey = 'asFMu6rJdA39jyE6ZwygWoqWWaPprdRv'
-const giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&q='+q;
 
-fetch(giphyUrl).then(function(res) {
-    return res.json()
-}).then(function(json) {
-    console.log(json.data[0].images.fixed_width.url)
-    const resultsEl = document.getElementById('results')
-    let resultsHTML = ''
-    json.data.forEach(function(obj) {
-        console.log(obj.images.fixed_width.url)
-        const url = obj.images.fixed_width.url
-        const width = obj.images.fixed_width.width
-        resultsHTML += '<img src="${url} width="${width}>'
+function randomGif(q){
+    const apiKey = 'asFMu6rJdA39jyE6ZwygWoqWWaPprdRv'
+    const giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + q;
+    
+    fetch(giphyUrl).then(function (res) {
+        return res.json()
+    }).then(function (json) {
+        console.log(json)
+        const resultsEl = document.getElementById('results')
+       
+        let url;
+        let resultsHTML = document.createElement('img');
+        const random = Math.floor(Math.random() * json.data.length)
+        const randomImg = json.data[random];
+    
+    
+        url = randomImg.images.fixed_width.url
+    
+        resultsHTML.setAttribute('src', url)
+        resultsHTML.setAttribute('width', '200')
+    
+        resultsEl.append(resultsHTML);
+    }).catch(function (err) {
+        console.log(err)
     })
+}
 
-    resultsEl.innerHTML
-}).catch(function(err) {
-    console.log(err.messsage)
-})
+
+// need to run if else based on if % change in ticker price works or not. 
+ randomGif('cats')
+// if (percentChange >0) {
+//     q= 'happy'
+//     randomGif
+// else {
+
+
+
+
